6753702dc61997d4136945df9d196dc1b440b6a5
Henrique de Lima Pessoa <henriquedelimapessoa@gmail.com>
2023-05-27 03:19:19 -0300

    bird position

--- src/Game/Components/Bird.tsx
+++ src/Game/Components/Bird.tsx
@@ -27,7 +27,7 @@ const Bird = () => {
       gameHasStarted={gameHasStarted}
       size={BIRD_SIZE}
       top={birdPosition}
-      left={GAME_WIDTH / 2}
+      left={(GAME_WIDTH / 2) - (BIRD_SIZE / 2)}
       angle={birdAngle}
     >
       <Bird_img variant={birdFrame} />
